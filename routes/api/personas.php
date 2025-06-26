f3122de8f54e17a4c61b9682f4ef1bc0af115297
Arista Soluciones <isc061990@outlook.com>
2025-06-25 22:57:51 -0600

    coreccion para route binding en personas

--- routes/api/personas.php
+++ routes/api/personas.php
@@ -5,8 +5,8 @@ Route::middleware(['auth:sanctum'])->prefix('personas')->name('personas.')->grou
 
     Route::get('', [App\Http\Controllers\PersonaController::class,'list'])->name('lista');
     Route::post('', [App\Http\Controllers\PersonaController::class,'create'])->name('registrar');
-    Route::patch('{id}', [App\Http\Controllers\PersonaController::class,'update'])->name('actualizar');
-    Route::delete('{id}', [App\Http\Controllers\PersonaController::class,'delete'])->name('eliminar');
+    Route::patch('{persona}', [App\Http\Controllers\PersonaController::class,'update'])->name('actualizar');
+    Route::delete('{persona}', [App\Http\Controllers\PersonaController::class,'delete'])->name('eliminar');
 
     //Route::put('{persona}/user', [App\Http\Controllers\PersonaController::class,'actualizarPassword'])->name('actualizar.user');
     //Route::delete('{persona}/user/{user}/desactivar', [App\Http\Controllers\PersonaController::class,'desactivarUsuario'])->name('desactivar.user');
