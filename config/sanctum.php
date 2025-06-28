645aae0886a6a120423b8f234469e2d91e52a653
Arista Soluciones <isc061990@outlook.com>
2025-06-28 13:31:07 -0600

    cmabios

--- config/sanctum.php
+++ config/sanctum.php
@@ -20,6 +20,7 @@ return [
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
+        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
 
     /*
